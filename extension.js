d276774ca6a3a29e68be3e452aa5a55c73ddbf02
Robert Konrad <robert@ktx-software.com>
2016-01-23 21:49:10 +0100

    Reload window when Kha project is created.

--- extension.js
+++ extension.js
@@ -2,8 +2,9 @@ var path = require('path');
 var vscode = require('vscode');
 
 function activate(context) {
-	var disposable = vscode.commands.registerCommand('extension.init', function () {
+	var disposable = vscode.commands.registerCommand('kha.init', function () {
 		require(path.join(vscode.extensions.getExtension('ktx.kha').extensionPath, 'Kha', 'Tools', 'khamake', 'init.js')).run('Project', vscode.workspace.rootPath, 'khafile.js');
+		vscode.commands.executeCommand('workbench.action.reloadWindow');
 		vscode.window.showInformationMessage('Kha project created.');
 	});
 	

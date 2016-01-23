4e48c3059e50b2d07299beb93419cb5c85a78fda
Robert Konrad <robert@ktx-software.com>
2016-01-23 21:39:41 +0100

    Added Init Kha Project command.

--- extension.js
+++ extension.js
@@ -1,30 +1,17 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+var path = require('path');
 var vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-kha" is now active!'); 
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	var disposable = vscode.commands.registerCommand('extension.init', function () {
+		require(path.join(vscode.extensions.getExtension('ktx.kha').extensionPath, 'Kha', 'Tools', 'khamake', 'init.js')).run('Project', vscode.workspace.rootPath, 'khafile.js');
+		vscode.window.showInformationMessage('Kha project created.');
 	});
 	
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
+
 exports.deactivate = deactivate;

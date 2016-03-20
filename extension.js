cbe8983e72cf14a80eb724aff4266215994a057b
Robert Konrad <robert@ktx-software.com>
2016-03-20 05:26:39 +0100

    Changed build command to use khamake.

--- extension.js
+++ extension.js
@@ -19,14 +19,73 @@ exports.activate = function (context) {
 	context.subscriptions.push(disposable);
 
 	disposable = vscode.commands.registerCommand('kha.compile', function () {
-		var exec = require('child_process').exec;
-		var cmd = 'haxe project-html5.hxml';
-		exec(cmd,{cwd: vscode.workspace.rootPath + '/build'}, function(error, stdout, stderr) {
-			if(error){vscode.window.showInformationMessage(error + " : " + stderr);}
+		const fs = require('fs');
+		fs.stat(path.resolve(vscode.workspace.rootPath, 'Kha'), (err, stats) => {
+			let options = {
+				from: vscode.workspace.rootPath,
+				to: path.join(vscode.workspace.rootPath, 'build'),
+				projectfile: 'khafile.js',
+				target: 'debug-html5',
+				vr: 'none',
+				pch: false,
+				intermediate: '',
+				graphics: 'direct3d9',
+				visualstudio: 'vs2015',
+				kha: '',
+				haxe: '',
+				ogg: '',
+				aac: '',
+				mp3: '',
+				h264: '',
+				webm: '',
+				wmv: '',
+				theora: '',
+				kfx: '',
+				krafix: '',
+				nokrafix: false,
+				embedflashassets: false,
+				compile: false,
+				run: false,
+				init: false,
+				name: 'Project',
+				server: false,
+				port: 8080,
+				debug: false,
+				silent: false
+			};
+			if (err == null) {
+				try {
+					require(path.join(vscode.workspace.rootPath, 'Kha/Tools/khamake/main.js'))
+						.run(options, {
+							info: message => {
+								console.log(message);
+							}, error: message => {
+								console.error(message);
+							}
+						}, function (name) { });
+				}
+				catch (error) {
+					console.error('Error: ' + error.toString());
+				}
+			}
+			else {
+				try {
+					require(path.join(findKha(), 'Tools/khamake/main.js'))
+						.run(options, {
+							info: message => {
+								console.log(message);
+							}, error: message => {
+								console.error(message);
+							}
+						}, function (name) { });
+				}
+				catch (error) {
+					console.error('Error: ' + error.toString());
+				}
+			}
 		});
-		
 	});
-	
+
 	context.subscriptions.push(disposable);
 };
 

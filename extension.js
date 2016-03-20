f492b59e7ce1a7f83cd4516045be779594f2e869
Robert Konrad <robert@ktx-software.com>
2016-03-20 05:03:31 +0100

    Added khaPath config option.

--- extension.js
+++ extension.js
@@ -1,18 +1,28 @@
+"use strict";
+
 var path = require('path');
 var vscode = require('vscode');
 
-function activate(context) {
-	var disposable = vscode.commands.registerCommand('kha.init', function () {
-		require(path.join(vscode.extensions.getExtension('ktx.kha').extensionPath, 'Kha', 'Tools', 'khamake', 'init.js')).run('Project', vscode.workspace.rootPath, 'khafile.js');
+function findKha() {
+	let khapath = vscode.workspace.getConfiguration('kha').khaPath;
+	if (khapath.length > 0) return khapath;
+	return path.join(vscode.extensions.getExtension('ktx.kha').extensionPath, 'Kha');
+}
+
+exports.activate = function (context) {
+	let disposable = vscode.commands.registerCommand('kha.init', function () {
+		require(path.join(findKha(), 'Tools', 'khamake', 'init.js')).run('Project', vscode.workspace.rootPath, 'khafile.js');
 		vscode.commands.executeCommand('workbench.action.reloadWindow');
 		vscode.window.showInformationMessage('Kha project created.');
 	});
-	
+
 	context.subscriptions.push(disposable);
-}
-exports.activate = activate;
+};
 
-function deactivate() {
-}
+exports.deactivate = function () {
 
-exports.deactivate = deactivate;
+};
+
+exports.findKha = function () {
+	return findKha();
+};

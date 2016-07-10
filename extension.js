91506e32eedcd122070517853a74e3f026484706
Robert Konrad <git2016@robdangero.us>
2016-07-10 18:24:18 +0200

    Export compile function.

--- extension.js
+++ extension.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const vscode = require('vscode');
 
+let channel = null;
+
 function findKha() {
 	let localkhapath = path.resolve(vscode.workspace.rootPath, 'Kha');
 	if (fs.existsSync(localkhapath)) return localkhapath;
@@ -14,6 +16,55 @@ function findKha() {
 
 function findFFMPEG() {
 	return vscode.workspace.getConfiguration('kha').ffmpeg;
+}
+
+function compile(target) {
+	let options = {
+		from: vscode.workspace.rootPath,
+		to: path.join(vscode.workspace.rootPath, 'build'),
+		projectfile: 'khafile.js',
+		target: target,
+		vr: 'none',
+		pch: false,
+		intermediate: '',
+		graphics: 'direct3d9',
+		visualstudio: 'vs2015',
+		kha: '',
+		haxe: '',
+		ogg: '',
+		aac: '',
+		mp3: '',
+		h264: '',
+		webm: '',
+		wmv: '',
+		theora: '',
+		kfx: '',
+		krafix: '',
+		ffmpeg: findFFMPEG(),
+		nokrafix: false,
+		embedflashassets: false,
+		compile: false,
+		run: false,
+		init: false,
+		name: 'Project',
+		server: false,
+		port: 8080,
+		debug: false,
+		silent: false
+	};
+	try {
+		require(path.join(findKha(), 'Tools/khamake/main.js'))
+		.run(options, {
+			info: message => {
+				channel.appendLine(message);
+			}, error: message => {
+				channel.appendLine(message);
+			}
+		}, function (name) { });
+	}
+	catch (error) {
+		channel.appendLine('Error: ' + error.toString());
+	}
 }
 
 function getTargets() {
@@ -44,7 +95,7 @@ function mapTarget(name) {
 	switch (name) {
 		case 'HTML5': return 'html5';
 		case 'HTML5 (native)': return 'html5-native';
-		case 'HTML5 Worker': return 'html5-worker';
+		case 'HTML5 Worker': return 'html5worker';
 		case 'Windows': return 'windows';
 		case 'Windows Universal': return 'windowsapp';
 		case 'macOS': return 'osx';
@@ -65,7 +116,7 @@ function mapTarget(name) {
 }
 
 exports.activate = function (context) {
-	let channel = vscode.window.createOutputChannel('Kha');
+	channel = vscode.window.createOutputChannel('Kha');
 
 	let disposable = vscode.commands.registerCommand('kha.init', function () {
 		require(path.join(findKha(), 'Tools', 'khamake', 'init.js')).run('Project', vscode.workspace.rootPath, 'khafile.js');
@@ -77,59 +128,9 @@ exports.activate = function (context) {
 
 	disposable = vscode.commands.registerCommand('kha.compile', function () {
 		vscode.window.showQuickPick(getTargets()).then(target => {
+			channel.show();
 			channel.appendLine('Compiling to ' + target);
-			const fs = require('fs');
-			fs.stat(path.resolve(vscode.workspace.rootPath, 'Kha'), (err, stats) => {
-				let options = {
-					from: vscode.workspace.rootPath,
-					to: path.join(vscode.workspace.rootPath, 'build'),
-					projectfile: 'khafile.js',
-					target: mapTarget(target),
-					vr: 'none',
-					pch: false,
-					intermediate: '',
-					graphics: 'direct3d9',
-					visualstudio: 'vs2015',
-					kha: '',
-					haxe: '',
-					ogg: '',
-					aac: '',
-					mp3: '',
-					h264: '',
-					webm: '',
-					wmv: '',
-					theora: '',
-					kfx: '',
-					krafix: '',
-					ffmpeg: findFFMPEG(),
-					nokrafix: false,
-					embedflashassets: false,
-					compile: false,
-					run: false,
-					init: false,
-					name: 'Project',
-					server: false,
-					port: 8080,
-					debug: false,
-					silent: false
-				};
-				try {
-					channel.show();
-					let success = require(path.join(findKha(), 'Tools/khamake/main.js'))
-						.run(options, {
-							info: message => {
-								channel.appendLine(message);
-							}, error: message => {
-								channel.appendLine(message);
-							}
-						}, function (name) { });
-					if (success) vscode.window.showInformationMessage('Compiled Kha project to ' + target + '.');
-					else vscode.window.showErrorMessage('Error compiling Kha project to ' + target + '.');
-				}
-				catch (error) {
-					channel.appendLine('Error: ' + error.toString());
-				}
-			});
+			compile(mapTarget(target));
 		});
 	});
 
@@ -149,7 +150,8 @@ exports.activate = function (context) {
 
 	let api = {
 		findKha: findKha,
-		findFFMPEG: findFFMPEG
+		findFFMPEG: findFFMPEG,
+		compile: compile
 	};
 
 	return api;

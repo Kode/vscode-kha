cf84c7863ce2a38f49076916955f67cf67301396
Robert Konrad <git2016@robdangero.us>
2016-07-10 03:55:05 +0200

    Check for local Kha right in findKha.

--- extension.js
+++ extension.js
@@ -1,9 +1,12 @@
-"use strict";
+'use strict';
 
-var path = require('path');
-var vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+const vscode = require('vscode');
 
 function findKha() {
+	let localkhapath = path.resolve(vscode.workspace.rootPath, 'Kha');
+	if (fs.existsSync(localkhapath)) return localkhapath;
 	let khapath = vscode.workspace.getConfiguration('kha').khaPath;
 	if (khapath.length > 0) return khapath;
 	return path.join(vscode.extensions.getExtension('ktx.kha').extensionPath, 'Kha');
@@ -58,35 +61,18 @@ exports.activate = function (context) {
 				debug: false,
 				silent: false
 			};
-			if (err == null) {
-				try {
-					require(path.join(vscode.workspace.rootPath, 'Kha/Tools/khamake/main.js'))
-						.run(options, {
-							info: message => {
-								console.log(message);
-							}, error: message => {
-								console.error(message);
-							}
-						}, function (name) { });
-				}
-				catch (error) {
-					console.error('Error: ' + error.toString());
-				}
+			try {
+				require(path.join(findKha(), 'Tools/khamake/main.js'))
+					.run(options, {
+						info: message => {
+							console.log(message);
+						}, error: message => {
+							console.error(message);
+						}
+					}, function (name) { });
 			}
-			else {
-				try {
-					require(path.join(findKha(), 'Tools/khamake/main.js'))
-						.run(options, {
-							info: message => {
-								console.log(message);
-							}, error: message => {
-								console.error(message);
-							}
-						}, function (name) { });
-				}
-				catch (error) {
-					console.error('Error: ' + error.toString());
-				}
+			catch (error) {
+				console.error('Error: ' + error.toString());
 			}
 		});
 	});

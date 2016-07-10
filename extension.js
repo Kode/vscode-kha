fc93c4a338a0d767717a434e00db1a97fba0a82d
Robert Konrad <git2016@robdangero.us>
2016-07-10 15:36:08 +0200

    Added target system chooser.

--- extension.js
+++ extension.js
@@ -16,7 +16,57 @@ function findFFMPEG() {
 	return vscode.workspace.getConfiguration('kha').ffmpeg;
 }
 
+function getTargets() {
+	return [
+		'HTML5',
+		//'HTML5 (native)',
+		//'HTML5 Worker',
+		'Windows',
+		'Windows Universal',
+		'macOS',
+		'Linux',
+		'Android',
+		'Android (native)',
+		'iOS',
+		'Raspberry Pi',
+		'Tizen',
+		'Flash',
+		'node.js',
+		'Unity',
+		'XNA',
+		'PlayStation Mobile',
+		'Java',
+		'WPF'
+	];
+}
+
+function mapTarget(name) {
+	switch (name) {
+		case 'HTML5': return 'html5';
+		case 'HTML5 (native)': return 'html5-native';
+		case 'HTML5 Worker': return 'html5-worker';
+		case 'Windows': return 'windows';
+		case 'Windows Universal': return 'windowsapp';
+		case 'macOS': return 'osx';
+		case 'Linux': return 'linux';
+		case 'Android': return 'android';
+		case 'Android (native)': return 'android-native';
+		case 'iOS': return 'ios';
+		case 'Raspberry Pi': return 'pi';
+		case 'Tizen': return 'tizen';
+		case 'Flash': return 'flash';
+		case 'node.js': return 'node';
+		case 'Unity': return 'unity';
+		case 'XNA': return 'xna';
+		case 'PlayStation Mobile': return 'psm';
+		case 'Java': return 'java';
+		case 'WPF': return 'wpf';
+	}
+}
+
 exports.activate = function (context) {
+	let channel = vscode.window.createOutputChannel('Kha');
+
 	let disposable = vscode.commands.registerCommand('kha.init', function () {
 		require(path.join(findKha(), 'Tools', 'khamake', 'init.js')).run('Project', vscode.workspace.rootPath, 'khafile.js');
 		vscode.commands.executeCommand('workbench.action.reloadWindow');
@@ -26,54 +76,60 @@ exports.activate = function (context) {
 	context.subscriptions.push(disposable);
 
 	disposable = vscode.commands.registerCommand('kha.compile', function () {
-		const fs = require('fs');
-		fs.stat(path.resolve(vscode.workspace.rootPath, 'Kha'), (err, stats) => {
-			let options = {
-				from: vscode.workspace.rootPath,
-				to: path.join(vscode.workspace.rootPath, 'build'),
-				projectfile: 'khafile.js',
-				target: 'debug-html5',
-				vr: 'none',
-				pch: false,
-				intermediate: '',
-				graphics: 'direct3d9',
-				visualstudio: 'vs2015',
-				kha: '',
-				haxe: '',
-				ogg: '',
-				aac: '',
-				mp3: '',
-				h264: '',
-				webm: '',
-				wmv: '',
-				theora: '',
-				kfx: '',
-				krafix: '',
-				ffmpeg: findFFMPEG(),
-				nokrafix: false,
-				embedflashassets: false,
-				compile: false,
-				run: false,
-				init: false,
-				name: 'Project',
-				server: false,
-				port: 8080,
-				debug: false,
-				silent: false
-			};
-			try {
-				require(path.join(findKha(), 'Tools/khamake/main.js'))
-					.run(options, {
-						info: message => {
-							console.log(message);
-						}, error: message => {
-							console.error(message);
-						}
-					}, function (name) { });
-			}
-			catch (error) {
-				console.error('Error: ' + error.toString());
-			}
+		vscode.window.showQuickPick(getTargets()).then(target => {
+			channel.appendLine('Compiling to ' + target);
+			const fs = require('fs');
+			fs.stat(path.resolve(vscode.workspace.rootPath, 'Kha'), (err, stats) => {
+				let options = {
+					from: vscode.workspace.rootPath,
+					to: path.join(vscode.workspace.rootPath, 'build'),
+					projectfile: 'khafile.js',
+					target: mapTarget(target),
+					vr: 'none',
+					pch: false,
+					intermediate: '',
+					graphics: 'direct3d9',
+					visualstudio: 'vs2015',
+					kha: '',
+					haxe: '',
+					ogg: '',
+					aac: '',
+					mp3: '',
+					h264: '',
+					webm: '',
+					wmv: '',
+					theora: '',
+					kfx: '',
+					krafix: '',
+					ffmpeg: findFFMPEG(),
+					nokrafix: false,
+					embedflashassets: false,
+					compile: false,
+					run: false,
+					init: false,
+					name: 'Project',
+					server: false,
+					port: 8080,
+					debug: false,
+					silent: false
+				};
+				try {
+					channel.show();
+					let success = require(path.join(findKha(), 'Tools/khamake/main.js'))
+						.run(options, {
+							info: message => {
+								channel.appendLine(message);
+							}, error: message => {
+								channel.appendLine(message);
+							}
+						}, function (name) { });
+					if (success) vscode.window.showInformationMessage('Compiled Kha project to ' + target + '.');
+					else vscode.window.showErrorMessage('Error compiling Kha project to ' + target + '.');
+				}
+				catch (error) {
+					channel.appendLine('Error: ' + error.toString());
+				}
+			});
 		});
 	});
 
